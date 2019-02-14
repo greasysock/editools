67bb8e2f66e7de2d3004ecee5a6d49d008e9553c
Chris <chris.gresock@gmail.com>
2019-02-14 14:09:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using EdiEngine;
+using EdiEngine.Runtime;
+
+namespace editools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader tIn = Console.In;
+            String ediData = tIn.ReadLine();
+
+            EdiDataReader r = new EdiDataReader();
+            EdiBatch ediBatch = r.FromString(ediData);
+
+            //control whether you need to accept all transaction or report error if such.
+            AckBuilderSettings ackSettings = new AckBuilderSettings(AckValidationErrorBehavour.AcceptAll, false, 100, 200);
+            var ack = new AckBuilder(ackSettings);
+
+            //create FA object structure
+            EdiBatch ackBatch = ack.GetnerateAcknowledgment(ediBatch);
+            string data = ack.WriteToString(ediBatch);
+            Console.WriteLine(data);
+        }
+    }
+}
